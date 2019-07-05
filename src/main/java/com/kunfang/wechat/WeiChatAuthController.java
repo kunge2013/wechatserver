c98236d7c85f54052f07d3da42748a5db37962cc
kunge2013 <543976974@QQ.com>
2019-07-06 01:28:42 +0800

    update

--- src/main/java/com/kunfang/wechat/WeiChatAuthController.java
+++ src/main/java/com/kunfang/wechat/WeiChatAuthController.java
@@ -2,6 +2,7 @@ package com.kunfang.wechat;
 
 import com.alibaba.druid.util.HttpClientUtils;
 import com.kunfang.wechat.service.ILoginHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,16 +13,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/login")
 public class WeiChatAuthController {
+    @Autowired
     private ILoginHandler handler;
     @RequestMapping(value = "/auth/{userName}/{passWord}/{code}", method = RequestMethod.GET)
-    public Object auth(@PathVariable("userName") String userName, @PathVariable("passWord") String passWord,@PathVariable("code") String code) {
+    public Object auth(@PathVariable("userName") String userName, @PathVariable("passWord") String passWord, @PathVariable("code") String code) {
         Map<String, Object> map = new HashMap<>();
         String appId = "";
         String appSecret = "";
         String openId =  handler.loginWeChat(appId, appSecret, code);
         map.put("success" , true);
         map.put("openId" , openId);
-
         return map;
     }
 }
