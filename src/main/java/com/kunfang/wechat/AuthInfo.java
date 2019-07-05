18f675271d3012478f1f0173a838ff1eae2971dc
kunge2013 <543976974@QQ.com>
2019-07-06 02:20:50 +0800

    update

--- src/main/java/com/kunfang/wechat/AuthInfo.java
+++ src/main/java/com/kunfang/wechat/AuthInfo.java
@@ -2,4 +2,23 @@ package com.kunfang.wechat;
 
 public class AuthInfo extends ResponseError {
 
+    private String session_key;
+
+    private String openId;
+
+    public String getSession_key() {
+        return session_key;
+    }
+
+    public void setSession_key(String session_key) {
+        this.session_key = session_key;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
 }
