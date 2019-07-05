96de09ea0fe326e4c0e70912ca2c9eb07ea22ee9
kunge2013 <543976974@QQ.com>
2019-07-06 00:17:51 +0800

    update

--- src/main/java/com/kunfang/ServletInitializer.java
+++ src/main/java/com/kunfang/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.kunfang;
 
-import javafx.application.Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
